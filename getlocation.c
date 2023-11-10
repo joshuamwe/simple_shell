43124067d9337167756f6145941b5844a977b839
joshuamwe <joshuajomwema2819@gmail.com>
2023-11-10 12:18:40 +0300

    shell

--- getlocation.c
+++ getlocation.c
@@ -3,65 +3,124 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+
+char *strdup_checked(const char *str);
+char *build_file_path(const char *directory, const char *command);
+int file_exists(const char *file_path, struct stat *buffer);
+char *search_in_path(const char *path_copy, const char *command);
 /**
- * get_location - main function
- * @command: parameter 1
+ * Duplicate a string and check for errors
+ * @str: parameter
+ * Return: duplicate
+ */
+char *strdup_checked(const char *str)
+{
+	char *duplicate = strdup(str);
+
+	if (!duplicate)
+	{
+		perror("strdup");
+	}
+
+	return (duplicate);
+}
+
+/**
+ * build_file_path - main function
+ * @directory: parameter 1
+ * @command: parameter 2
+ * Return: 0
+ */
+char *build_file_path(const char *directory, const char *command)
+{
+	int directory_length = strlen(directory);
+	int command_length = strlen(command);
+
+	char *file_path = malloc(directory_length + command_length + 2);
+
+	if (!file_path)
+	{
+		perror("malloc");
+
+		return (NULL);
+	}
+
+	strcpy(file_path, directory);
+	strcat(file_path, "/");
+	strcat(file_path, command);
+
+	return (file_path);
+}
+
+/**
+ * file_exists - Check if a file exists at the given path
+ * @file_path: parameter 1
+ * @buffer: parameter 2
+ * Return: 0
+ */
+int file_exists(const char *file_path, struct stat *buffer)
+{
+	return (stat(file_path, buffer) == 0);
+}
+
+/**
+ * search_in_path -Search location of a command in PATH
+ * @path_copy: paramet 1
+ * @command: para 2
  * Return: NULL
  */
-char *get_location(char *command)
+char *search_in_path(const char *path_copy, const char *command)
 {
-	char *path, *path_copy, *path_token, *file_path;
-	int command_length;
-	struct stat buffer;
-
-	path = getenv("PATH");
-	if (path)
+	char *path_token;
+	char *file_path;
+	for (path_token = strtok(strdup_checked(path_copy), ":"); path_token != NULL; path_token = strtok(NULL, ":"))
 	{
-		path_copy = strdup(path);
+		file_path = build_file_path(path_token, command);
 
-		if (!path_copy)
+		if (!file_path)
 		{
-			perror("strdup");
 			return (NULL);
 		}
 
-		command_length = strlen(command);
-
-		for (path_token = strtok(path_copy, ":"); path_token != NULL; path_token = strtok(NULL, ":"))
+		if (file_exists(file_path, NULL))
 		{
-			int directory_length = strlen(path_token);
-			file_path = malloc(command_length + directory_length + 2);
-
-			if (!file_path)
-			{
-				perror("malloc");
-
-				free(path_copy);
-				return (NULL);
-			}
-
-			strcpy(file_path, path_token);
-			strcat(file_path, "/");
-			strcat(file_path, command);
-
-			if (stat(file_path, &buffer) == 0)
-			{
-				free(path_copy);
-				return (file_path);
-			}
-			else
-			{
-				free(file_path);
-			}
+			return (file_path);
 		}
-		free(path_copy);
-
-		if (stat(command, &buffer) == 0)
+		else
 		{
-			return (strdup(command));
+			free(file_path);
 		}
 	}
-
 	return (NULL);
+}
+
+/**
+ * Main function to get the location of a command
+ */
+char *get_location(char *command) {
+    char *path = getenv("PATH");
+
+    if (path) {
+        char *path_copy = strdup_checked(path);
+
+        if (!path_copy) {
+            return NULL;
+        }
+
+        char *result = search_in_path(path_copy, command);
+
+        free(path_copy);
+
+        if (result) {
+            return result;
+        }
+        struct stat buffer;
+
+        if (file_exists(command, &buffer)) {
+            return strdup_checked(command);
+        }
+    }
+
+    return NULL;
 }
 
